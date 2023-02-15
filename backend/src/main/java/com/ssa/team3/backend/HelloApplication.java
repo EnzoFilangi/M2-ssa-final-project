583949c0715c038ef47d676fc61654e2574f6e2b
EnzoFilangi <enzo.filangi@efrei.net>
2023-02-15 11:01:24 +0100

    Project skeleton

---
+++ backend/src/main/java/com/ssa/team3/backend/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.ssa.team3.backend;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
