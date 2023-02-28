cdf1c02118b7aa00037b4cbaf38cafef401b431b
EnzoFilangi <enzo.filangi@efrei.net>
2023-02-28 10:34:47 +0100

    Added CORS headers

---
+++ backend/src/main/java/com/ssa/team3/backend/controller/http/CorsFilter.java
@@ -0,0 +1,21 @@
+package com.ssa.team3.backend.controller.http;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+
+import java.io.IOException;
+
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+    // From : https://www.baeldung.com/cors-in-jax-rs
+    @Override
+    public void filter(ContainerRequestContext requestContext,
+                       ContainerResponseContext responseContext) throws IOException {
+        responseContext.getHeaders().add(
+                "Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add(
+                "Access-Control-Allow-Credentials", "true");
+    }
+}
