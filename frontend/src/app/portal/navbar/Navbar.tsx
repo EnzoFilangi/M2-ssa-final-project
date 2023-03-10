2e07c77ba45485729ff978af6bbada5d254d2524
EnzoFilangi <enzo.filangi@efrei.net>
2023-03-10 21:20:20 +0100

    Switched place for "disconnect" button and name of current user

--- frontend/src/app/portal/navbar/Navbar.tsx
+++ frontend/src/app/portal/navbar/Navbar.tsx
@@ -17,12 +17,12 @@ export function Navbar({user, logout}: NavbarProps) {
                     <ul className="flex flex-col mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
                         <li>
                             <a href="#"
-                               className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" onClick={logout}>Se Déconnecter</a>
-
+                               className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">{user.firstName}&nbsp;{user.lastName}</a>
                         </li>
                         <li>
                             <a href="#"
-                               className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">{user.firstName}&nbsp;{user.lastName}</a>
+                               className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" onClick={logout}>Se Déconnecter</a>
+
                         </li>
                     </ul>
                 </div>
